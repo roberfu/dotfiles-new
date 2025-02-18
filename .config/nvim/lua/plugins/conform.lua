3e1b45d9d5ad974954e6b7f98c6d6bdbbf8b75ab
Roberto Fuentes <roberfuentesg@gmail.com>
2025-02-18 12:45:45 -0300

    Update files

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -32,6 +32,7 @@ return { -- autoformat
         end,
         formatters_by_ft = {
             lua = { "stylua" },
+            json = { "biome" },
             -- Conform can also run multiple formatters sequentially
             -- python = { "isort", "black" },
             --
